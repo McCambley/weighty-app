d8e52fe2a6c9208d1936abb88f6debe49a166f82
McCambley <jakemccamb16@gmail.com>
2024-05-12 02:57:33 -0400

    feat: Add plates, bar weight, and target weight inputs to index screen

--- app/index.tsx
+++ app/index.tsx
@@ -1,6 +1,9 @@
-import { Text, View } from "react-native";
+import { Text, View, TextInput, Pressable, StyleSheet } from "react-native";
+import { useState } from "react";
 
 export default function Index() {
+  const [state, setState] = useState("hello");
+  const [barWeight, setBarWeight] = useState(45);
   return (
     <View
       style={{
@@ -9,7 +12,73 @@ export default function Index() {
         alignItems: "center",
       }}
     >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+      <Text style={{ fontSize: 16 }}>Plates</Text>
+      <Text style={{ fontSize: 16 }}>Bar Weight</Text>
+      <View style={{ display: "flex", flexDirection: "row", gap: 8 }}>
+        <Pressable
+          style={[
+            styles.button,
+            { backgroundColor: barWeight === 45 ? "red" : "white" },
+          ]}
+          onPress={() => setBarWeight(45)}
+        >
+          <Text>45</Text>
+        </Pressable>
+        <Pressable
+          style={[
+            styles.button,
+            { backgroundColor: barWeight === 20 ? "red" : "white" },
+          ]}
+          onPress={() => setBarWeight(20)}
+        >
+          <Text>20</Text>
+        </Pressable>
+        <Pressable
+          style={[
+            styles.button,
+            { backgroundColor: barWeight === 0 ? "red" : "white" },
+          ]}
+          onPress={() => setBarWeight(0)}
+        >
+          <Text>0</Text>
+        </Pressable>
+      </View>
+      <Text style={{ fontSize: 16 }}>Target Weight</Text>
+      <TextInput
+        style={{
+          borderWidth: 1,
+          paddingTop: 2,
+          paddingBottom: 2,
+          paddingLeft: 16,
+          paddingRight: 16,
+          borderRadius: 10,
+        }}
+        placeholder="hello"
+        value={state}
+        onChange={(e) => {
+          setState(e.target.value);
+        }}
+      />
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  button: {
+    borderWidth: 1,
+    paddingTop: 2,
+    paddingBottom: 2,
+    paddingLeft: 16,
+    paddingRight: 16,
+    borderRadius: 10,
+  },
+  buttonSelected: {
+    borderWidth: 1,
+    paddingTop: 2,
+    paddingBottom: 2,
+    paddingLeft: 16,
+    paddingRight: 16,
+    borderRadius: 10,
+    backgroundColor: "red",
+  },
+});
