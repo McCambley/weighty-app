a3daf972095e121b78c9a3b564eb40693ddfc324
McCambley <jakemccamb16@gmail.com>
2024-05-13 21:27:25 -0400

    ✨ Create calculator functionality

--- components/Calculator.tsx
+++ components/Calculator.tsx
@@ -1,9 +1,44 @@
 import { Text, View, TextInput, Pressable, StyleSheet } from "react-native";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
-export default function Calculator() {
-  const [state, setState] = useState("hello");
-  const [barWeight, setBarWeight] = useState(45);
+type PlateValue = 45 | 35 | 25 | 10 | 5 | 2.5;
+type PlateTotal = { weight: number; count: number };
+const plateOptions: PlateValue[] = [45, 35, 25, 10, 5, 2.5];
+
+export default function Calculator(): JSX.Element {
+  const [inputValue, setInputValue] = useState<string>("");
+  const [barWeight, setBarWeight] = useState<number>(45);
+  const [weightTotals, setWeightTotals] = useState<PlateTotal[]>([]);
+  console.log(weightTotals);
+  useEffect(() => {
+    calculatePlates();
+  }, [inputValue, barWeight]);
+
+  function handleInputChange(text: string): void {
+    if (!isNaN(Number(text))) {
+      setInputValue(text);
+    }
+  }
+
+  function calculatePlates() {
+    // Create placeholder to store calculated values
+    const totals: PlateTotal[] = [];
+    if (!inputValue) return;
+    // Copy the input weight
+    let remainingWeight = Math.max(parseInt(inputValue) - barWeight, 0) / 2;
+    // For each weight in descending order
+    for (let i = 0; i < plateOptions.length; i++) {
+      // See how many of each weight can go into the input amount
+      const count = Math.floor(remainingWeight / plateOptions[i]);
+      if (count) {
+        // Store that value
+        totals.push({ count, weight: plateOptions[i] });
+        // Calculate the remainder
+        remainingWeight = remainingWeight % (count * plateOptions[i]);
+      }
+    }
+    setWeightTotals(totals);
+  }
   return (
     <View
       style={{
@@ -45,6 +80,7 @@ export default function Calculator() {
       </View>
       <Text style={{ fontSize: 16 }}>Target Weight</Text>
       <TextInput
+        keyboardType="numeric"
         style={{
           borderWidth: 1,
           paddingTop: 2,
@@ -54,11 +90,16 @@ export default function Calculator() {
           borderRadius: 10,
         }}
         placeholder="hello"
-        value={state}
-        onChange={(e) => {
-          setState(e.target.value);
-        }}
+        value={inputValue}
+        onChangeText={handleInputChange}
       />
+      {weightTotals.map((weight) => {
+        return (
+          <Text key={weight.weight}>
+            {weight.weight}: {weight.count}
+          </Text>
+        );
+      })}
     </View>
   );
 }
