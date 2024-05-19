2806d7fd1c9561ed397289afcf4d0059d92f8e11
McCambley <jakemccamb16@gmail.com>
2024-05-19 14:49:25 -0400

    Update head element in html

--- app/+html.tsx
+++ app/+html.tsx
@@ -18,6 +18,26 @@ export default function Root({ children }: PropsWithChildren) {
 
         {/* Link the PWA manifest file. */}
         <link rel="manifest" href="/manifest.json" />
+        {/* <!-- Define the name of the app --> */}
+        <meta
+          name="apple-mobile-web-app-title"
+          content="Plate Calculator"
+        ></meta>
+
+        {/* <!-- Define the start URL --> */}
+        <meta name="apple-mobile-web-app-capable" content="yes"></meta>
+
+        {/* <!-- Define the status bar style (default, black, or black-translucent) --> */}
+        <meta
+          name="apple-mobile-web-app-status-bar-style"
+          content="black"
+        ></meta>
+
+        {/* <!-- Link to the icon --> */}
+        <link rel="apple-touch-icon" href="/apple-touch-icon.png"></link>
+
+        {/* <!-- Define the display (standalone, fullscreen, or minimal-ui) --> */}
+        <meta name="mobile-web-app-capable" content="yes"></meta>
 
         {/*
           Disable body scrolling on web. This makes ScrollView components work closer to how they do on native.
